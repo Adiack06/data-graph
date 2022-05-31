e165ff2847a2e2cd904f5e41578cbf639c2d38c4
josh <joshmills2006@outlook.com>
2022-05-31 16:54:15 +0100

    before fixes

--- graphing.py
+++ graphing.py
@@ -1,12 +1,15 @@
 import pygame
-screenx = 2560+1920
-screen = pygame.display.set_mode((screenx,1000))
+screenx = 2560
+screeny = 1000
+screen = pygame.display.set_mode((screenx,screeny))
 running: bool = True
 days =[]
 lines = 0
 lastx = 0
 lasty = 0
-
+verticlescale = 40
+horizontalscale = 4
+xlocation = 0
 affile = open("tmax", "r")
 for i in affile.readlines():
     days.append(i)
@@ -16,29 +19,44 @@ for i in affile.readlines():
 affile.close()
 
 while running:
+    keys = pygame.key.get_pressed()
+    keymod = pygame.key.get_mods
     lines = int(lines)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((100,100,100))
+    screen.fill((0,0,0))
     n=0
     lastx = 0
     lasty = 1000
     peaky =0
+    for i in range(1000):
+        pygame.draw.line(screen, (50, 50, 50), (0, (screeny - (5 * (i * verticlescale))/4)),(screenx, (screeny - (5 * (i * verticlescale))/4)), 2)
+    for i in range(1000):
+        pygame.draw.line(screen, (50, 50, 50),(50 * (i * horizontalscale)+xlocation,0 ),(50 * (i * horizontalscale)+xlocation, screeny), 2)
 
     for i in days:
         n+=1
         n=int(n)
-        pygame.draw.line(screen, (255, 255, 255), ((screenx/lines)*n,1000-(float(i)*20)), (lastx,lasty), 2)
-        lastx = (screenx/lines)*n
-        lasty = 1000-(float(i)*20)
-        if 1000-(float(i)*20) > peaky:
-            peaky = 1000-(float(i)*20)
-    for i in range(10):
-        pygame.draw.line(screen,(255, 255, 255),(0,i*100), (screenx, i*100), 2)
+        pygame.draw.line(screen, (70, 70, 70), ((n*horizontalscale)+xlocation,1000-(float(i)*verticlescale)), (lastx,lasty), 4)
+        lastx = (n*horizontalscale)+xlocation
+        lasty = 1000-(float(i)*verticlescale)
 
 
-    print(peaky)
+    if pygame.mouse.get_pressed(5)[2]:
+        if pygame.mouse.get_pressed(5)[3]:
+            horizontalscale+= 0.025
+        if pygame.mouse.get_pressed(5)[4]:
+            horizontalscale -= 0.025
 
 
+    if pygame.mouse.get_pressed(5)[0]:
+        if pygame.mouse.get_pressed(5)[3]:
+            verticlescale += 0.25
+        if pygame.mouse.get_pressed(5)[4]:
+            verticlescale -= 0.25
+    if keys[pygame.K_d]:
+        xlocation -= 5
+    if keys[pygame.K_a]:
+        xlocation += 5
     pygame.display.flip()
